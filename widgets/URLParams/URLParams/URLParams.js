56202ef553634ccf704e85fa984b68449acba21c
Scott Davis <stdavis@utah.gov>
2018-09-25 13:26:01 -0600

    feat: Implement x, y, and scale URL parameters Related to #17

--- widgets/URLParams/URLParams/URLParams.js
+++ widgets/URLParams/URLParams/URLParams.js
@@ -1,11 +1,56 @@
 import _WidgetBase from 'dijit/_WidgetBase';
 import declare from 'dojo/_base/declare';
+import hash from 'dojo/hash';
+import ioQuery from 'dojo/io-query';
+import Point from 'esri/geometry/Point';
 
 
 export default declare([_WidgetBase], {
-  constructor() {
-    console.log('URLParams:constructor', arguments);
+  postCreate() {
+    console.log('URLParams:postCreate', arguments);
+
+    this.initializeRouter();
 
     this.inherited(arguments);
+  },
+
+  initializeRouter() {
+    console.log('URLParams:initializeRouter', arguments);
+
+    const params = ioQuery.queryToObject(hash());
+
+    if (params.scale && params.x && params.y) {
+      this.map.setScale(parseInt(params.scale, 10));
+      this.map.centerAt(new Point({
+        x: parseInt(params.x, 10),
+        y: parseInt(params.y, 10),
+        spatialReference: { wkid: this.map.spatialReference.wkid }
+      })).then(this.wireEvents.bind(this));
+    } else {
+      this.wireEvents();
+    }
+  },
+
+  wireEvents() {
+    console.log('URLParams:wireEvents', arguments);
+
+    this.own(
+      this.map.on('extent-change', this.onMapExtentChange.bind(this))
+    );
+  },
+
+  onMapExtentChange(event) {
+    console.log('URLParams:onMapExtentChange', arguments);
+
+    const center = event.extent.getCenter();
+    if (center.x && center.y) {
+      const newProperties = Object.assign(ioQuery.queryToObject(hash()), {
+        x: Math.round(center.x),
+        y: Math.round(center.y),
+        scale: Math.round(this.map.getScale())
+      });
+
+      return hash(ioQuery.objectToQuery(newProperties), true);
+    }
   }
 });
