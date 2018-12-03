46a8767e6ac8239568a1e06f5a53dd20276b6c91
Scott Davis <stdavis@utah.gov>
2018-12-03 12:56:35 -0700

    feat: auto-expand widget on map click Closes #23

--- widgets/ProjectInfo/Widget.js
+++ widgets/ProjectInfo/Widget.js
@@ -1,6 +1,7 @@
 import declare from 'dojo/_base/declare';
 import BaseWidget from 'jimu/BaseWidget';
 import ProjectInfo from './ProjectInfo/ProjectInfo';
+import PanelManager from 'jimu/PanelManager';
 
 
 export default declare([BaseWidget], {
@@ -11,5 +12,22 @@ export default declare([BaseWidget], {
       config: this.config,
       map: this.map
     }, this.widgetContainer);
+
+    this.map.on('click', this.onMapClick.bind(this));
+  },
+
+  onMapClick() {
+    // auto-open this widget's panel and close all others except BetterAbout
+    const panelManager = PanelManager.getInstance();
+
+    panelManager.panels.forEach(panel => {
+      if (panel.config.id === this.id) {
+        if (panel.state === 'closed') {
+          panelManager.openPanel(panel);
+        }
+      } else if (panel.windowState !== 'maximized') {
+        panelManager.closePanel(panel);
+      }
+    });
   }
 });
